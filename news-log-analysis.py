696ab178bb331c1c16e3a9d793d8dafa0d7107da
vagrant <vagrant@vagrant.vm>
2018-05-16 13:31:27 +0000

    Added print_query_results function

--- news-log-analysis.py
+++ news-log-analysis.py
@@ -11,31 +11,38 @@ query_1 = (
     "like concat('%', articles.slug, '%') "
     "group by articles.title, log.path order by views desc limit 3")
 
-question_2 = "Who are the most popular article authors of all time?"
-query_2 = (
-    "")
+#question_2 = "Who are the most popular article authors of all time?"
+#query_2 = (
+    #"")
 
-question_3 = "On which days did more than 1% of requests lead to errors?"
+#question_3 = "On which days did more than 1% of requests lead to errors?"
+#query_3 = (
+    #"")
 
-def runQuery(sql_query):
+def run_query(query):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute(sql_query)
+    c.execute(query)
     results = c.fetchall()
-    db.close
     return results
+    db.close
 
-result_1 = runQuery(query_1)
-#result_2 = runQuery(query_2)
-#result_3 = runQuery(query_3)
+result_1 = run_query(query_1)
+#result_2 = run_query(query_2)
+#result_3 = run_query(query_3)
 
-def printResult():
+def print_query_results(results):
+    print (results[1])
+    for index, results in enumerate(results[0]):
+        print (
+            "\t", index+1, "-", str(results[0]),
+            "\t - ", str(results[1]), "views")
 
 print(question_1)
-printResult(result_1)
-    
+print_query_results(result_1)
+
 # print(question_2)
 # printResult(result_2)
-    
+        
 # print(question_3)
 # printResult(result_3)
