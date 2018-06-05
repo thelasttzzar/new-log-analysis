0af3cab73b019bf83de0fd95447ae5ba745d08cd
vagrant <vagrant@vagrant.vm>
2018-06-05 14:16:54 +0000

    Added 2nd query

--- newslog.py
+++ newslog.py
@@ -13,18 +13,23 @@ query_1 = (
     "group by articles.title, log.path "
     "order by views desc limit 3")
 
-#question_2 = "Who are the most popular article authors of all time?"
-#query_2 = (
-    #"")
+question_2 = "Who are the most popular article authors of all time?"
+query_2 = (
+    "select authors.name, count(*) as views from articles "
+    "inner join authors on articles.author = authors.id "
+    "inner join log on log.path like concat('%', articles.slug, '%') "
+    "where log.status like '%200%' "
+    "group by authors.name "
+    "order by views desc")
 
 #question_3 = "On which days did more than 1% of requests lead to errors?"
 #query_3 = (
     #"")
 
 
-def connect(database_name="news"):
+def connect(dbname="news"):
     try:
-        db = psycopg2.connect("dbname={}".format(database_name))
+        db = psycopg2.connect("dbname={}".format(dbname))
         cursor = db.cursor()
         return db, cursor
     except:
@@ -41,16 +46,16 @@ def run_query(query):
 def print_results(query_results):
     print (query_results[1])
     for index, results in enumerate(query_results[0]):
-        print "\t", index+1, "-", results[0],"\t - ", str(results[1]), "views"
+        print "\t", results[0],"\t - ", str(results[1]), "views"
 
 
 if __name__ == '__main__':
     # define/store results
     three_most_popular_articles = run_query(query_1), question_1
-    # most_popular_authors = run_query(query_2), question_2
+    most_popular_authors = run_query(query_2), question_2
     # days_errors_exceed_one-percent = run_query(query_3), question_3
     
     # print results
     print_results(three_most_popular_articles)
-    # print_results(most_popular_authors)
+    print_results(most_popular_authors)
     # print_results(days_errors_exceed_one-percent)
